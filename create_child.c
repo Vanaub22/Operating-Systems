7625478a4f8d2b006f12cce72fef572d693781ac
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2023-09-05 13:12:30 +0530

    Assignment-5 Child, Orphan and Zombie Process

---
+++ create_child.c
@@ -0,0 +1,10 @@
+// AIM: To create a child process and print the PID & PPID from both the parent and child processes
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+void main(){
+    int pid=fork();
+    sleep(10);
+    if(pid==0) printf("\nThis is from the child process. PID: %d PPID: %d\n",getpid(),getppid());
+    else printf("\nThis is from the parent process. PID: %d PPID: %d\n",getpid(),getppid());
+}// end of main
