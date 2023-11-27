fcb561cf06b4e99481f3483d204766aad617a4d9
Anuvab Chakravarty <anucbs2018@gmail.com>
2023-11-28 01:45:43 +0530

    Removed executables

--- OS_LAB_Final_Practice/Assignment-5/zombie.c
+++ OS_LAB_Final_Practice/Assignment-5/zombie.c
@@ -12,16 +12,16 @@ void main() {
 		exit(0);
 	}
 	else if(id==0) {
-		printf("This is the child process with PID: %d and PPID: %d",getpid(),getppid());
+		printf("This is the child process with PID: %d and PPID: %d\n",getpid(),getppid());
 		sleep(5);
 		printf("The child process is now killed.\nThe child process is now a Zombie Process.\n");
 	}
 	else {
-		printf("This is the parent process with PID: %d and PPID: %d",getpid(),getppid());
+		printf("This is the parent process with PID: %d and PPID: %d\n",getpid(),getppid());
 		sleep(10);
 		wait(NULL);
 		printf("The child process is no longer a zombie process\n");
 		sleep(5);
-		printf("The parent process is killed.\n");		
+		printf("The parent process is killed.\n");
 	}
 }
